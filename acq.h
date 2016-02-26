b8039fba81b1c151bbbaeefade5a3ccde79196dd
root <https://st02@github.com>
2016-02-26 14:18:49 +0100

    second commit

---
+++ acq.h
@@ -0,0 +1,17 @@
+#ifndef ACQ_H
+#define ACQ_H
+#include <stdbool.h>
+#include <stdint.h>
+void my_ready_cb(bool success, uint8_t att_ecode, void *user_data);
+void notify_cb(uint16_t value_handle, const uint8_t *value, uint16_t length, void *user_data);
+
+static float dt = 1000;
+static float in_curr[3]={0.f,0.f,0.f};
+static float in_prev[3]={0.F,0.F,0.F};
+static float out_curr[3]={0.F,0.F,0.F};
+static float out_prev[3]={0.F,0.F,0.F};
+static int time;
+
+
+
+#endif
---
+++ btgatt-client.h
@@ -0,0 +1,31 @@
+#ifndef BTGATT_CLIENT_H
+#define BTGATT_CLIENT_H
+
+#define COLOR_OFF	"\x1B[0m"
+#define COLOR_RED	"\x1B[0;91m"
+#define COLOR_GREEN	"\x1B[0;92m"
+#define COLOR_YELLOW	"\x1B[0;93m"
+#define COLOR_BLUE	"\x1B[0;94m"
+#define COLOR_MAGENTA	"\x1B[0;95m"
+#define COLOR_BOLDGRAY	"\x1B[1;30m"
+#define COLOR_BOLDWHITE	"\x1B[1;37m"
+
+#define PRLOG(...) \
+	printf(__VA_ARGS__); print_prompt();
+
+struct client {
+	int fd;
+	struct bt_att *att;
+	struct gatt_db *db;
+	struct bt_gatt_client *gatt;
+
+	unsigned int reliable_session_id;
+};
+
+void print_prompt(void);
+void cmd_write_value(struct client *cli, char *cmd_str);
+void cmd_read_value(struct client *cli, char *cmd_str);
+void cmd_register_notify(struct client *cli, char *cmd_str);
+FILE *fp;
+
+#endif
